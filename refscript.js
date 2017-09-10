853beb17257fbd5af4607bc37cd65ac68af5ae34
rishabh9028 <31797064+rishabh9028@users.noreply.github.com>
2017-09-11 00:28:58 +0530

    Add files via upload

---
+++ refscript.js
@@ -0,0 +1,45 @@
+function Submit(frm){
+
+if(frm.checkValidity()){
+
+	var win=window.open("","Submit","width=300,height=500");
+	win.document.write("<html><head><title>Submit Data</title></head><body>");
+	win.document.write("<h1>Submit Data</h1>");
+	// win.document.write("<p>First Name is :"+ frm[0].value +"</p>");
+	win.document.write("<p>First Name is :"+ frm.fname.value +"</p>");
+	win.document.write("<p>Last Name is :"+ frm[1].value +"</p>");
+	win.document.write("<p>Contact Number is :"+ frm[2].value +"</p>");
+	win.document.write("<p>Email Address is :"+ frm[3].value +"</p>");
+	win.document.write("<p>Date of Birth is :"+ frm[4].value +"</p>");
+
+	win.document.write("<p>Location is :"
+	+ frm[5].options[frm[5].selectedIndex].text +"</p>");
+	win.document.write("<p>State is :"+ frm[5].value+"</p>");
+		win.document.write("</body></html>");
+	}
+	
+}
+	// var city;
+	// var state;
+	// if(frm[5].selectedIndex==0)
+		// {
+		// city="Chennai";
+		// state="Tamil Nadu";
+		// }
+	// if(frm[5].selectedIndex==1)
+		// {
+		// city="Mumbai";
+		// state="Maharastra";
+		// }
+	// if(frm[5].selectedIndex==2)
+		// {
+		// city="Pune";
+		// state="Maharastra";
+		// }
+	// if(frm[5].selectedIndex==3)
+		// {
+		// city="Banglore";
+		// state="karanataka";
+		// }
+	// win.document.write("<p>City is :"+ city +"</p>");
+	// win.document.write("<p>State is :"+ state +"</p>");
